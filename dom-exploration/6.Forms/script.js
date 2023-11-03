175819e6386e557dc2c7304a5093ddffa0311d2c
Alec VM <alecvm24@hotmail.com>
2023-11-03 15:23:10 +0100

    add keyup listener on the first and second input field

--- dom-exploration/6.Forms/script.js
+++ dom-exploration/6.Forms/script.js
@@ -1,2 +1,23 @@
 /*
 */
+// put keyup listener on the first input field
+let textBox = document.querySelector('#firstname');
+textBox.addEventListener('keyup', function (){
+    let textEntered = textBox.value;
+    let messageElement = document.querySelector ('#display-firstname');
+    messageElement.innerText = textEntered
+})
+
+//  add keyup listener on the second field
+const ageInput = document.getElementById('age');
+const HardTruthSection = document.getElementById('a-hard-truth');
+
+ageInput.addEventListener('keyup', function(){
+    const age = parseInt(ageInput.value, 10);
+    if (age < 18){
+        HardTruthSection.style.visibility = 'hidden';
+    }
+    else{ 
+        HardTruthSection.style.visibility = 'visible';
+    }
+});
